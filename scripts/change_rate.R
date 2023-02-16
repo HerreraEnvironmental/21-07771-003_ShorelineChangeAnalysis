dab8f0152876417ad75ac9bd7148733a5c18d714
Regina Lionheart <rlionheart@herrerainc.com>
2023-02-16 14:25:21 -0800

    Euclidean conditional slope colors added.

--- scripts/change_rate.R
+++ scripts/change_rate.R
@@ -5,7 +5,8 @@
 
 source("scripts/load_packages.R")
 
-profile.pattern <- "prof_6|prof_7|prof_8|prof_9"
+#profile.pattern <- "prof_6|prof_7|prof_8|prof_9|prof_17|prof_41"
+profile.pattern <- "prof"
 source("scripts/import_profiles.R")
 
 ## Import erosion file for Base Point data
@@ -27,7 +28,12 @@ complete.profile <- profile.erosion %>%
   group_by(profile, year, season) %>% # can probably drop season
   mutate(X_midpoint = ((min(x) + max(x))/2)) %>%
   mutate(Y_midpoint = ((min(y) + max(y))/2)) %>%
-  mutate(euc_avg = mean(euc_dist))
+  mutate(euc_avg = mean(euc_dist)) %>%
+  group_by(profile) %>%
+  mutate(slope = ((max(y) - min(y)) / (max(x) - min(x)))) %>%
+  mutate(slope = ifelse(slope > 1, "positive", "negative")) %>%
+  ##
+  drop_na()
 
 ## Prelim visual of data vs BasePoint
 g <- ggplot(data = complete.profile) +
@@ -37,21 +43,19 @@ g <- ggplot(data = complete.profile) +
 g
 
 # With color by year and best fit
-partial.visual <- complete.profile %>%
-  filter(profile %in% c(6, 7, 8, 9))
+partial.visual <- complete.profile #%>%
+  #filter(profile %in% c(6, 7, 8, 9, 17, 41))
 
 ggplot(data = partial.visual) +
   geom_point(aes(x = x, y = y), alpha = 0.2) +
   geom_point(aes(x = BasePoint_X, y = BasePoint_Y), color = "black", size = 3) +
   geom_point(aes(x = X_midpoint, y = Y_midpoint), color = "blue", size = 3) +
-  #geom_smooth(method=lm, se = FALSE, aes(x = x, y = y, color = year, weight = 2)) +
   ggtitle(paste("Profile:", profile.pattern))
 
 ## Euclidean distances
 euclidean <- partial.visual %>%
-  ungroup() %>%
-  select(profile, year, BasePoint_X, BasePoint_Y, X_midpoint, Y_midpoint) %>%
   group_by(profile, year) %>%
+  select(profile, year, slope, BasePoint_X, BasePoint_Y, X_midpoint, Y_midpoint) %>%
   mutate(X_midpoint = mean(X_midpoint),
          Y_midpoint = mean(Y_midpoint)) %>%
   unique() %>%
@@ -61,13 +65,13 @@ euclidean$year <- factor(euclidean$year, levels =  c("97", "98", "99","00", "01"
                                                      "11", "12", "13", "14", "15", "16", "17",
                                                      "18", "19", "20", "21", "22"))
 
-
 ## Visualize euclidean distance from average Euclidean distance of each year
-ggplot(euclidean, aes(year, euc_dist, group = profile)) +
-  facet_wrap(~profile) +
-  geom_col(position = position_dodge(width = 1)) +
-  geom_line(aes(group = profile), position = position_dodge(width = 1), size = 2) +
-  geom_smooth(method = "lm") +
+ggplot(euclidean, aes(year, euc_dist, group = profile, color=slope)) +
+  facet_wrap(~profile, scales = "free") +
+  geom_col(position = position_dodge(width = 0.5)) +
+  geom_line(aes(group = slope), position = position_dodge(width = 1),
+            size = 2) +
+  geom_smooth(method = "lm", se = TRUE, color="black") +
   theme(legend.position = "none") +
   ggtitle(paste("Profile", profile.pattern, ": Midpoint Euclidean Distance from Base Point"))
 
@@ -76,6 +80,16 @@ ggplot(euclidean, aes(year, euc_dist,
                       group = profile, color = factor(profile))) +
   geom_point() +
   geom_line()
+
+
+
+ggplot(dat %>% group_by(pairs) %>%
+         mutate(slope = (value[t==2] - value[t==1])/(2-1)),
+       aes(t, value, group=pairs, linetype=group, colour=slope > 0)) +
+  geom_point() +
+  geom_line()
+
+
 
 
 #### CHECK OUT LATER
