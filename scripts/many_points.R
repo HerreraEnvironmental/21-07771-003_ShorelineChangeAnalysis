645ab84ab7507871b9ef3af219cb621865a6f20f
Regina Lionheart <rlionheart@herrerainc.com>
2023-03-01 10:19:20 -0800

    Midway through lm dataframe build.

--- scripts/many_points.R
+++ scripts/many_points.R
@@ -24,7 +24,7 @@ CalculateChangeRates <- function(x, y) {
 
 ## Take n Euclidean points along profiles and use that as the change rate.
 
-profile.pattern <- "prof_22"
+profile.pattern <- "prof_6"
 year.pattern <- c("00")
 
 source("scripts/load_packages.R")
@@ -45,52 +45,63 @@ profile.erosion <- read_csv("data_raw/ProfilesForErosion.csv",
 ## Combine and add euclidean distance
 complete.profile <- profile.erosion %>%
   full_join(profiles.df, by = "profile") %>%
-  select(profile, Park, X_BasePoint, Y_BasePoint, season:z)
+  select(profile, Park, X_BasePoint, Y_BasePoint, season:z) %>%
+  drop_na()
 complete.profile$year <- factor(complete.profile$year, levels =  c("97", "98", "99","00", "01", "02", "03",
                                                      "04", "05", "06", "07", "08", "09", "10",
                                                      "11", "12", "13", "14", "15", "16", "17",
                                                      "18", "19", "20", "21", "22"))
 
-## Make a linear model and extract slope and intercept 
-mydata <- complete.profile %>% filter(year %in% year.pattern)
-linear.model <- lm(data = mydata, x ~ y)
-
-intercept <- coef(linear.model)[1] # Intercept
-slope <- coef(linear.model)[2] # slope
-
-y <- (slope*min(mydata$x)) + intercept
-
-
 ## Plot a single profile and year with a regression line. 
-profile.lm <- ggplot(data = complete.profile %>% filter(year %in% year.pattern),
+lm.profile <- ggplot(data = complete.profile %>% filter(year %in% year.pattern),
        aes(x = x, y = y, group = year)) +
   facet_wrap(~year) +
   geom_point() +
   stat_smooth(method = lm, se = FALSE) +
   #theme(axis.text = element_blank()) +
   ggtitle(paste("Profile:", profile.pattern, "Year:", year.pattern))
-profile.lm
+lm.profile
 
-profile.lm.df <- ggplot_build(profile.lm)$data[[2]][, c("x","y")]
+## Create a df of xy coordinates for each profile and year grouping
+test <- complete.profile %>%
+  group_by(profile, year) %>%
+  do(model = lm(y ~ x, data = .)) %>%
+  mutate(intercept = coef(model)[1],
+         slope = coef(model)[2])
 
 
 
-# %>%
-#   mutate(x_max = max(x),
-#          y_max = max(y)) %>%
-#   mutate(x_midpoint = ((min(x) + max(x))/2),
-#          y_midpoint = ((min(y) + max(y))/2)) %>%
-#   mutate(x_quartile1 = ((min(x) + x_midpoint)/2),
-#          y_quartile1 = ((min(y) + y_midpoint)/2)) %>%
-#   mutate(x_quartile3 = ((x_midpoint + max(x))/2),
-#          y_quartile3 = ((y_midpoint + max(y))/2)) 
+# Small sanity check: Doing it for one profile
+profile.lm.df <- ggplot_build(lm.profile)$data[[2]][, c("x","y")] 
+# Quick model: find y at x position
+linear.model <- lm(data = profile.lm.df, y ~ x)
+intercept <- coef(linear.model)[1] # Intercept
+slope <- coef(linear.model)[2] # slope
+y <- (slope*min(profile.lm.df$x)) + intercept
 
+
+## Add points of interest on the regression lines
+
+plot(profile.lm.df$x, profile.lm.df$y)
+distances.df <- profile.lm.df %>%
+  mutate(year = year.pattern,
+         profile = str_extract_all(profile.pattern, "\\(?[0-9,.]+\\)?")[[1]]) %>%
+  mutate(x_min = min(x),
+         y_min = min(y)) %>%
+  mutate(x_max = max(x),
+         y_max = max(y)) %>%
+  mutate(x_midpoint = ((min(x) + max(x))/2),
+         y_midpoint = ((min(y) + max(y))/2)) %>%
+  mutate(x_quartile1 = ((min(x) + x_midpoint)/2),
+         y_quartile1 = ((min(y) + y_midpoint)/2)) %>%
+  mutate(x_quartile3 = ((x_midpoint + max(x))/2),
+         y_quartile3 = ((y_midpoint + max(y))/2))
 
 
 ## Plot
-ggplot(data = complete.profile %>% filter(year %in% year.pattern)) +
+ggplot(data = distances.df %>% filter(year %in% year.pattern)) +
   facet_wrap(~year) +
-  geom_point(aes(x = x, y = y), alpha = 0.5, fill = season) +
+  geom_point(aes(x = x, y = y), alpha = 0.5) +
   geom_point(aes(x = x_min, y = y_min), color = "red", size = 3) +
   geom_point(aes(x = x_quartile1, y = y_quartile1), color = "orange", size = 3) +
   geom_point(aes(x = x_midpoint, y = y_midpoint), color = "green", size = 3) +
@@ -99,8 +110,9 @@ ggplot(data = complete.profile %>% filter(year %in% year.pattern)) +
   ggtitle(paste("Profile:", profile.pattern, "Year:", year.pattern))
 
 # Change rates?...
-change <- complete.profile %>% 
-  select(profile, Park, year, everything(), -c(x, y, z, season)) %>%
+change <- distances.df %>% 
+  #select(profile, Park, year, everything(), -c(x, y, z, season)) %>%
+  select(-x, -y) %>%
   unique() %>%
   group_by(profile) %>% 
   mutate(min_rate = CalculateChangeRates(x_min, y_min)) %>%
