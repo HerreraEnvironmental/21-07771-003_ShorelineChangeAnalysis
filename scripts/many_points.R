b9f0004b8071c5d2b192711aa1d16f21d4c025ec
Regina Lionheart <rlionheart@herrerainc.com>
2023-03-01 15:58:25 -0800

    Change rate graph working

--- scripts/many_points.R
+++ scripts/many_points.R
@@ -8,7 +8,7 @@
 #   xval = (x - lag(x))/lag(x)
 #   yval = (y - lag(y))/lag(y)
 #   rate = xval/yval
-#   
+# 
 #   return(rate)
 # }
 
@@ -17,7 +17,7 @@ CalculateChangeRates <- function(x, y) {
   xval = (max(x) - min(x)/min(x))
   yval = (max(y) - min(y)/min(y))
   rate = xval/yval
-  
+
   return(rate)
 }
 
@@ -31,7 +31,7 @@ MakeRateDF <- function(df, description) {
 
 ## Take quartile points along profiles and use euclidean distance to BP as the change rate.
 
-profile.pattern <- "prof_6"
+profile.pattern <- "prof_17"
 year.pattern <- c("00")
 
 source("scripts/src/load_packages.R")
@@ -102,12 +102,16 @@ single.POI.df <- complete.profile %>%
   mutate(x_max = max(x),
          y_max = (slope*max(x)) + intercept) %>%
   mutate(x_midpoint = ((x_min + x_max)/2),
-         y_midpoint = (((slope*min(x)) + intercept) + ((slope*max(x)) + intercept))/2) %>%
+         y_midpoint = (((slope*min(x)) + intercept) +
+                         ((slope*max(x)) + intercept))/2) %>%
   mutate(x_quartile1 = ((min(x) + ((x_min + x_max)/2))/2),
-         y_quartile1 = (((slope*min(x)) + intercept) + (((slope*min(x)) + intercept) + ((slope*max(x)) + intercept))/2)/2) %>%
-  
+         y_quartile1 = (((slope*min(x)) + intercept) + 
+                          (((slope*min(x)) + intercept) + 
+                             ((slope*max(x)) + intercept))/2)/2) %>%
   mutate(x_quartile3 = ((x_midpoint + max(x))/2),
-         y_quartile3 = ((((((slope*min(x)) + intercept) + ((slope*max(x)) + intercept))/2)) + ((slope*max(x)) + intercept))/2)
+         y_quartile3 = ((((((slope*min(x)) + intercept) + 
+                             ((slope*max(x)) + intercept))/2)) + 
+                          ((slope*max(x)) + intercept))/2)
 
 single.POI.plot <- ggplot(data = single.POI.df) +
   facet_wrap(~year) +
@@ -123,33 +127,92 @@ single.POI.plot
 
 
 ## Acquire slope and intercept for all profiles/years
+complete.lm.df <- complete.profile %>%
+  group_by(profile, year) %>%
+  do(model = lm(y ~ x, data = .)) %>%
+  mutate(intercept = coef(model)[1],
+         slope = coef(model)[2]) 
 
-# Change rates?...
-change <- points.of.interest.df %>% 
-  select(profile, Park, year, everything(), -c(x, y, z, season)) %>%
-  #select(-x, -y) %>%
+## Calculate min, quartile1, mid, quartile3, and max values for each profile and year
+complete.POI.df <- complete.profile %>%
+  left_join(complete.lm.df %>% select(-model), by = c("profile", "year")) %>%
+  group_by(profile, year) %>%
+  mutate(x_min = min(x),
+         y_min = (slope*min(x)) + intercept) %>%
+  mutate(x_max = max(x),
+         y_max = (slope*max(x)) + intercept) %>%
+  mutate(x_midpoint = ((x_min + x_max)/2),
+         y_midpoint = (((slope*min(x)) + intercept) +
+                         ((slope*max(x)) + intercept))/2) %>%
+  mutate(x_quartile1 = ((min(x) + ((x_min + x_max)/2))/2),
+         y_quartile1 = (((slope*min(x)) + intercept) + 
+                          (((slope*min(x)) + intercept) + 
+                             ((slope*max(x)) + intercept))/2)/2) %>%
+  mutate(x_quartile3 = ((x_midpoint + max(x))/2),
+         y_quartile3 = ((((((slope*min(x)) + intercept) + 
+                             ((slope*max(x)) + intercept))/2)) + 
+                          ((slope*max(x)) + intercept))/2) %>%
+  drop_na()
+
+# avgtest <- complete.POI.df %>%
+#   select(profile, year, contains("_")) %>%
+#   unique() %>%
+#   rowwise() %>%
+#   mutate(xavg = mean(x_min, x_max, x_quartile1, x_quartile3, x_midpoint)) %>%
+#   mutate(yavg = mean(y_min, y_max, y_quartile1, y_quartile3, y_midpoint))
+
+## Calculate euclidean distances between each point of interest and the base point.
+euclidean.distances <- complete.POI.df %>% 
+  select(profile, Park, year, everything(), -c(x, y, z, season, slope, intercept)) %>%
   unique() %>%
-  group_by(profile) %>% 
-  mutate(min_rate = CalculateChangeRates(x_min, y_min)) %>%
-  mutate(quart1_rate = CalculateChangeRates(x_quartile1, y_quartile1)) %>%
-  mutate(mid_rate = CalculateChangeRates(x_midpoint, y_midpoint)) %>%
-  mutate(quart3_rate = CalculateChangeRates(x_quartile3, y_quartile3)) %>%
-  mutate(max_rate = CalculateChangeRates(x_max, y_max))
+  ungroup() %>%
+  mutate(min_euc_BP = sqrt(((X_BasePoint - x_min)^2) + ((Y_BasePoint -  y_min)^2))) %>%
+  mutate(q1_euc_BP = sqrt(((X_BasePoint - x_quartile1)^2) + ((Y_BasePoint -  y_quartile1)^2))) %>%
+  mutate(mid_euc_BP = sqrt(((X_BasePoint - x_midpoint)^2) + ((Y_BasePoint -  y_midpoint)^2))) %>%
+  mutate(q3_euc_BP = sqrt(((X_BasePoint - x_quartile3)^2) + ((Y_BasePoint -  y_quartile3)^2))) %>%
+  mutate(max_euc_BP = sqrt(((X_BasePoint - x_max)^2) + ((Y_BasePoint -  y_max)^2)))
 
+ggplot(data = euclidean.distances, aes(x = year, y = min_euc_BP)) +
+  geom_bar(position = "dodge", stat = "identity", alpha = 0.5) +
+  ggtitle(paste("Profile", profile.pattern))
 
-min_df <- MakeRateDF(change, "min")
-quart1_df <- MakeRateDF(change, "quart1")
-mid_df <- MakeRateDF(change, "mid")
-quart3_df <- MakeRateDF(change, "quart3")
-max_df <- MakeRateDF(change, "max")
-  
-change.plot <- min_df %>%
-  rbind(quart1_df) %>%
-  rbind(mid_df) %>%
-  rbind(quart3_df) %>%
-  rbind(max_df) 
-  
+## Calculate rates of change  
+euclidean.rates <- euclidean.distances %>%
+  mutate(min_rate = (min_euc_BP/lag(min_euc_BP) - 1) * 100) #%>%
+  # mutate(min_rate = 100 * (min_euc_BP - lag(min_euc_BP))/lag(min_euc_BP)) %>%
+  # mutate(q1_rate = 100 * (q1_euc_BP - lag(q1_euc_BP))/lag(q1_euc_BP)) %>%
+  # mutate(mid_rate = 100 * (mid_euc_BP - lag(mid_euc_BP))/lag(mid_euc_BP)) %>%
+  # mutate(q3_rate = 100 * (q3_euc_BP - lag(q3_euc_BP))/lag(q3_euc_BP)) %>%
+  # mutate(max_rate = 100 * (max_euc_BP - lag(max_euc_BP))/lag(max_euc_BP))
 
-ggplot(change.plot, aes(x=year, y=rate, fill = position)) +
-  geom_col(position = "dodge", stat = "identity") +
+## Change rates to dfs for plot
+min_rate_df <- euclidean.rates %>%
+  select(profile, year, rate = min_rate) %>%
+  mutate(position = "min")
+
+# q1_rate_df <- euclidean.distances %>%
+#   select(profile, year, rate = q1_rate) %>%
+#   mutate(position = "q1")
+# 
+# mid_rate_df <- euclidean.distances %>%
+#   select(profile, year, rate = mid_rate) %>%
+#   mutate(position = "mid")
+# 
+# q3_rate_df <- euclidean.distances %>%
+#   select(profile, year, rate = q3_rate) %>%
+#   mutate(position = "q3")
+# 
+# max_rate_df <- euclidean.distances %>%
+#   select(profile, year, rate = max_rate) %>%
+#   mutate(position = "max")
+
+## Combine
+euc.plot <- min_rate_df %>%
+  rbind(q1_rate_df) %>%
+  rbind(mid_rate_df) %>%
+  rbind(q3_rate_df) %>%
+  rbind(max_rate_df)
+
+ggplot(data = euc.plot, aes(x = year, y = rate, fill = position)) +
+  geom_bar(position = "dodge", stat = "identity", alpha = 0.5) +
   ggtitle(paste("Profile", profile.pattern))
