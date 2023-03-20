2fc1b4c6fba2f1b576e1b42bc079c4ba7c09ee37
Regina Lionheart <rlionheart@herrerainc.com>
2023-03-20 16:35:26 -0700

    Finally got closer to correct order.

--- scripts/3d_time_series.R
+++ scripts/3d_time_series.R
@@ -4,7 +4,7 @@
 ## Shoreline Conservation Areas, Washington State Parks
 
 
-profile.pattern <- "prof_16"
+profile.pattern <- "prof_17"
 source("scripts/src/import_profiles.R")
 
 
@@ -14,44 +14,32 @@ source("scripts/src/import_profiles.R")
 ## Isolate one season, one year
 prof.data <- profiles.df %>%
   drop_na() %>%
-  mutate(year = factor(year, levels =  c("98", "99","00", "01", "02", "03",
+  mutate(year = factor(year, levels =  c("97", "98", "99","00", "01", "02", "03",
                                          "04", "05", "06", "07", "08", "09", "10",
                                          "11", "12", "13", "14", "15", "16", "17",
                                          "18", "19", "20", "21", "22"))) %>%
-  filter(season == "f") #%>%
-  #filter(!year %in% c("98", "99"))
+  filter(season == "f")# %>%
+  # filter(!year %in% c("98", "99"))
 
-
-## Plot
-marker <- list(size = 3, shape = 1)
-
-profileplot <- plot_ly(prof.data, x = ~x, y = ~y, z = ~z,
-                       color = ~year, marker = marker, hoverinfo = "text", 
-                       text = ~paste("</br> Year: ", year)) %>%
-  add_markers() %>%
-  layout(
-    scene = list(xaxis = list(title = "x"),
-                 yaxis = list(title = "y"),
-                 zaxis = list(title = "z")),
-    title = list(text = paste("Profile:", profile.pattern, "Years:"), y = 0.9),
-    legend = levels(year))
-
-profileplot
 
 
 ## Attempt to redo
 
 my.data <- prof.data %>%
   select(-profile, -season, -y) %>%
-  rename(y = year)
+  rename(y = year) %>%
+  arrange(y)
 
-my.data$y <- factor(my.data$y, levels =  c("98", "99","00", "01", "02", "03",
-                                           "04", "05", "06", "07", "08", "09", "10",
-                                           "11", "12", "13", "14", "15", "16", "17",
-                                           "18", "19", "20", "21", "22"))
-
+my.data$y <- as.numeric(my.data$y)
 
 prof.fig <- plot_ly(my.data, x = ~x, y = ~y, z = ~z, 
-                  type = "scatter3d", mode = "lines", 
-                  color=~year.list)
+                    type = "scatter3d", mode = "lines",
+                    color=~y) %>%
+  layout(yaxis = list(categoryorder = "array",
+                      categoryarray = c("97", "98", "99","00", "01", "02", "03",
+                                        "04", "05", "06", "07", "08", "09", "10",
+                                        "11", "12", "13", "14", "15", "16", "17",
+                                        "18", "19", "20", "21", "22")))
+         
 suppressWarnings(print(prof.fig))
+
