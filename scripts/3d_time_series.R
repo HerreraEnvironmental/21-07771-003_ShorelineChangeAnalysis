c57ae9e3a604e02dbdaa5990106c94eeda629f2a
Regina Lionheart <rlionheart@herrerainc.com>
2023-03-20 17:14:50 -0700

    Mostly fixed axes and order.

--- scripts/3d_time_series.R
+++ scripts/3d_time_series.R
@@ -4,7 +4,7 @@
 ## Shoreline Conservation Areas, Washington State Parks
 
 
-profile.pattern <- "prof_17"
+profile.pattern <- "prof_22"
 source("scripts/src/import_profiles.R")
 
 
@@ -18,28 +18,19 @@ prof.data <- profiles.df %>%
                                          "04", "05", "06", "07", "08", "09", "10",
                                          "11", "12", "13", "14", "15", "16", "17",
                                          "18", "19", "20", "21", "22"))) %>%
-  filter(season == "f")# %>%
-  # filter(!year %in% c("98", "99"))
-
-
+  filter(season == "f")
 
 ## Attempt to redo
 
 my.data <- prof.data %>%
   select(-profile, -season, -y) %>%
-  rename(y = year) %>%
-  arrange(y)
+  #rename(y = year) %>%
+  group_by(year) %>%
+  arrange(year)
 
-my.data$y <- as.numeric(my.data$y)
-
-prof.fig <- plot_ly(my.data, x = ~x, y = ~y, z = ~z, 
+prof.fig <- plot_ly(my.data, x = ~x, y = ~as.numeric(year), z = ~z, 
                     type = "scatter3d", mode = "lines",
-                    color=~y) %>%
-  layout(yaxis = list(categoryorder = "array",
-                      categoryarray = c("97", "98", "99","00", "01", "02", "03",
-                                        "04", "05", "06", "07", "08", "09", "10",
-                                        "11", "12", "13", "14", "15", "16", "17",
-                                        "18", "19", "20", "21", "22")))
-         
+                    width = 3, color=~year) 
+
 suppressWarnings(print(prof.fig))
 
