543719f20bf5a428d51b3f350a521aab4ae9c7e1
Regina Lionheart <rlionheart@herrerainc.com>
2023-04-05 15:41:47 -0700

    Fixed through examine_profiles.R

--- scripts/examine_profiles.R
+++ scripts/examine_profiles.R
@@ -13,28 +13,25 @@ print("Fullnames of unique profiles:")
 print(full.names)
 print("Unique numbers of profiles:")
 print(numbers.only)
+print("Missing profiles from sequential data:")
+setdiff(1:54, numbers.only)
 
 
 ## Import csv with geographic profile locations
 ## Import erosion file for Base Point data
-profile.erosion <- read_csv("data_raw/ProfilesForErosion.csv", 
-                            col_names = c("profile", "Park"), 
-                            col_select = (1:2),
-                            skip = 3, show_col_types = FALSE) %>%
-  drop_na()
-profile.OBA <- read_csv("data_raw/OBAProfiles.csv", 
-                        col_names = c("OBA", "profile", "Notes"), 
-                        col_select = c("profile", "OBA"),
-                        skip = 1, show_col_types = FALSE) %>%
-  separate_longer_delim(profile, ",") %>%
-  mutate(profile = as.numeric(gsub(" ", "", profile))) %>%
-  mutate(OBA = replace(OBA, OBA == "One Missing at 13?", "Ocean City"))
+# profile.erosion <- read_csv("data_raw/ProfilesForErosion.csv", 
+#                             col_names = c("profile", "Park"), 
+#                             col_select = (1:2),
+#                             skip = 3, show_col_types = FALSE) %>%
+#   drop_na()
 
-# complete.geo.profiles <- profile.OBA %>% 
-#   full_join(profile.erosion, by = "profile") %>%
-#   arrange(profile) %>%
-#   unite(col = "location", c(OBA, Park), sep = ", ", na.rm = TRUE) %>%
-#   complete(profile = 1:max(profile), fill = list(profile = 0))
+# profile.OBA <- read_csv("data_raw/OBAProfiles.csv", 
+#                         col_names = c("OBA", "profile", "Notes"), 
+#                         col_select = c("profile", "OBA"),
+#                         skip = 1, show_col_types = FALSE) %>%
+#   separate_longer_delim(profile, ",") %>%
+#   mutate(profile = as.numeric(gsub(" ", "", profile))) %>%
+#   mutate(OBA = replace(OBA, OBA == "One Missing at 13?", "Ocean City"))
 
 complete.geo.profiles <- read_csv("data_raw/NANOOS_WCEHA_validation.csv",
                       col_names = c("profile", "WCEHA", "location", "notes"),
