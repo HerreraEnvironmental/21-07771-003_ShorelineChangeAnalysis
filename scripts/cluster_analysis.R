bb168c4768dea840721a585a92cd4f46bc2fadb1
Regina Lionheart <rlionheart@herrerainc.com>
2023-04-06 12:04:35 -0700

    Remove dumbbell, assign most clusters.

--- scripts/cluster_analysis.R
+++ scripts/cluster_analysis.R
@@ -1,26 +1,56 @@
 ## Clustering analysis to determine subreaches
 
 ## Load and prepare data.
-## Import midpoint euclidean data and arrange so that rows are observations
-## and columsn are variables.
+## Import midpoint euclidean data. Order the profiles by geographic location,
+## not by whatever order it was in. 
 df <- read_csv("data_secondary/profiles_to_cluster.csv", show_col_types = FALSE) %>%
   mutate(year = factor(year, levels =  c("97", "98", "99","00", "01", "02", "03",
                                          "04", "05", "06", "07", "08", "09", "10", 
                                          "11", "12", "13", "14", "15", "16", "17",
                                          "18", "19", "20", "21", "22"))) %>%
+  mutate(profile = factor(profile, levels = c("1", "2", "3", "4", "5", "6", "7",
+                                              "8", "9", "10", "48", "11", "12",
+                                              "13", "14", "15", "16", "17", "18",
+                                              "19", "20", "21", "22", "23", "24",
+                                              "25", "26", "27", "28", "29", "30",
+                                              "31", "32", "33", "34", "35", "36",
+                                              "37", "49", "38", "50", "39", "40",
+                                              "51", "52", "41", "53", "54", "42",
+                                              "43", "44", "45", "46", "47"))) %>%
   select(profile:year, euc_dist_to_BP) %>%
   unique() %>%
+  arrange(profile)
+
+
+## Clustering is very sensitive to outliers, so we will remove them for now. 
+## Profiles 21 - 26 will be considered their own subreach, according to the 
+## below boxplot. 
+ggplot(df, aes(x = factor(profile), y = euc_dist_to_BP, group = profile)) +
+  geom_boxplot(outlier.color = "red") +
+  theme(panel.grid = element_line(color = "grey",
+                                     size = 0.01))
+
+## Munge data so that rows are observations and columns are variables.
+df.arranged <- df %>%
+  filter(!profile %in% c("21", "22", "23", "24", "25", "26")) %>%
   pivot_wider(names_from = c(Park, profile), values_from = euc_dist_to_BP) %>%
   column_to_rownames(var = "year") %>%
   t()
 
 ## Clustering cannot be performed on missing data. NA data needs to be removed.
-df.drop <- df[-c(1, 24, 46, 47),-c(1:3, 5, 14:16)]
-#df.drop <- na.omit(df)
+## Locate which rows to drop
+missing.rows <- which(rownames(df.arranged) %in% c("Haynisisoos Park, North Beach_1",
+                                                   "Del Rey Beach Rd OBA_46", 
+                                                   "Seaside Beach_47"))
+
+df.drop <- df.arranged[-c(missing.rows), -c(1:3, 5, 14:16)]
 
 ## Since the two variables do not have the same units, one may have more weight.
 ## Scale the data to compare variables independent of units.
 df.scaled <- scale(df.drop)
+
+new.rownames <- gsub(".*_","",rownames(df.scaled))
+row.names(df.scaled) <- new.rownames
 
 # HCA ---------------------------------------------------------------------
 # Hierarchical agglomerative clustering
@@ -41,14 +71,13 @@ plot(df.hclust, main = "Dendogram using 'single' distance method")
 barplot(df.hclust$height,
         names.arg = (nrow(df.scaled) - 1):1,
         main = "Barplot of Dendogram Heights, 'single' distance method")
-max(diff(df.hclust$height))
-abline(h = 2.831486, col = "blue")
+abline(h = 1.5, col = "blue")
 
 
 ## Largest, most conservative grouping is 3. Next best is 12.
 plot(df.hclust)
 rect.hclust(df.hclust,
-            k = 2, # k is used to specify the number of clusters, taken from previous steps.
+            k = 5, # k is used to specify the number of clusters, taken from previous steps.
             border = "blue")
 
   
@@ -84,20 +113,39 @@ res.hc <- df.scaled %>%
   dist(method = "euclidean") %>% 
   hclust(method = "ward.D2") 
 
-kmeans.dendogram <- fviz_dend(res.hc, k = 3, ## Assigning 4 clusters based on viewing the graph.
+kmeans.dendogram <- fviz_dend(res.hc, k = 4, ## Assigning 4 clusters based on viewing the graph.
           cex = 0.5,
-          k_colors = c("#2E9FDF", "#00AFBB", "#E7B800", "tomato1"),
+          k_colors = c("#2E9FDF", "#36A886", "#E7B800", "tomato1"),
           color_labels_by_k = TRUE,
           rect = TRUE)
 kmeans.dendogram
 
 
 # Assign clusters based on the last HCA --------------------------------------------------------
-
+## Add back in profiles 21 - 25
 df.clustered <- cutree(res.hc, k = 4) %>%
   as.data.frame() %>%
-  rownames_to_column(var = "Park_profile") %>%
-  separate(Park_profile, into = c("Park", "profile"), sep = "_") %>%
-  rename(cluster_id = 3) %>%
-  select(profile, Park, cluster_id)
+  rownames_to_column(var = "profile") %>%
+  rename(cluster_id = 2) %>%
+  full_join(df %>% select(profile, Park, euc_dist_to_BP), 
+            by = "profile") %>%
+  select(profile, Park, cluster_id, euc_dist_to_BP) %>%
+  unique() %>%
+  mutate(profile = factor(profile, levels = c("1", "2", "3", "4", "5", "6", "7",
+                                              "8", "9", "10", "48", "11", "12",
+                                              "13", "14", "15", "16", "17", "18",
+                                              "19", "20", "21", "22", "23", "24",
+                                              "25", "26", "27", "28", "29", "30",
+                                              "31", "32", "33", "34", "35", "36",
+                                              "37", "49", "38", "50", "39", "40",
+                                              "51", "52", "41", "53", "54", "42",
+                                              "43", "44", "45", "46", "47")))
 
+
+plt <- ggplot(df.clustered) +
+  geom_col(aes(euc_dist_to_BP, profile, 
+               group = factor(profile), fill = factor(cluster_id)), 
+           width = 0.6) +
+  scale_y_discrete(limits = rev, position = "right")
+
+plt
