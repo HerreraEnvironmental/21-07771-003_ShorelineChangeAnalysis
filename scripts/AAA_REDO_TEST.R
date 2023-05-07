d22b78a7fdbd7aa7df0afbceab019617e86ceb58
Regina Lionheart <rlionheart@herrerainc.com>
2023-05-07 15:55:25 -0700

    Commit before checking out pre-bug.

--- scripts/AAA_REDO_TEST.R
+++ scripts/AAA_REDO_TEST.R
@@ -2,6 +2,8 @@ original <- read.csv("archive/data_secondary_OG/profiles_with_clusters.csv")
 
 redo <- read.csv("data_secondary/REDOprofiles_with_clusters.csv")
 
+setdiff(original, redo)
 
-t <- original %>% left_join(redo) %>%
+
+t <- original %>% rbind(redo) %>%
   filter(profile %in% c("11", "40"))
